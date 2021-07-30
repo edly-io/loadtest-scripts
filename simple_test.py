7656fb36fb0f5b51b39d3cd229e0920ebb948bb2
fayyazahmed66 <fayyaz.ahmed@arbisoft.com>
2021-07-30 09:37:05 +0500

    task added for all pages

--- simple_test.py
+++ simple_test.py
@@ -1,12 +1,33 @@
 from locust import HttpUser, task, between
-
 class QuickstartUser(HttpUser):
     wait_time = between(3, 6)
-
-    @task
+    @task(1)
     def index_page(self):
         self.client.get("/")
-
-    @task(3)
+    @task(1)
     def view_contactus(self):
         self.client.get("/contact/")
+
+    @task(3)
+    def view_courses(self):
+        self.client.get("/courses/")    
+
+    @task(1)
+    def view_programs(self):
+        self.client.get("/programs/")  
+
+    @task(1)
+    def view_subscriptions(self):
+        self.client.get("/subscriptions/")    
+
+    @task(1)
+    def view_blogs(self):
+        self.client.get("/blogs/")  
+
+    @task(1)
+    def view_instructors(self):
+        self.client.get("/instructors/") 
+
+    @task(1)
+    def view_aboutus(self):
+        self.client.get("/about-us/")   
