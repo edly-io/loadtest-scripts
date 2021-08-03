f25c73320c0950217d51126748c0d3ca72ec8d5f
fayyazahmed66 <fayyaz.ahmed@arbisoft.com>
2021-08-03 17:30:05 +0500

    remove trailing spacing

--- simple_test.py
+++ simple_test.py
@@ -1,7 +1,7 @@
 from locust import HttpUser, task, between
 class QuickstartUser(HttpUser):
     wait_time = between(3, 6)
-    
+
     @task(1)
     def index_page(self):
         self.client.get("/")
@@ -12,28 +12,28 @@ class QuickstartUser(HttpUser):
 
     @task(3)
     def view_courses(self):
-        self.client.get("/courses/")    
+        self.client.get("/courses/")
 
     @task(1)
     def view_programs(self):
-        self.client.get("/programs/")  
+        self.client.get("/programs/")
 
     @task(1)
     def view_subscriptions(self):
-        self.client.get("/subscriptions/")    
+        self.client.get("/subscriptions/")
 
     @task(1)
     def view_blogs(self):
-        self.client.get("/blogs/")  
+        self.client.get("/blogs/")
 
     @task(1)
     def view_instructors(self):
-        self.client.get("/instructors/") 
+        self.client.get("/instructors/")
 
     @task(1)
     def view_aboutus(self):
-        self.client.get("/about-us/")   
+        self.client.get("/about-us/")
 
     @task(1)
     def view_courses_detail(self):
-        self.client.get("/course/sprint-82-course/")      
+        self.client.get("/course/sprint-82-course/")
